76f11d71de79ec07f1849200ae2cfae00ef1278a
MuhammadNur <delusionboys974@gmail.com>
2024-11-07 23:46:21 +0700

    add CORS middleware

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"github.com/codingsluv/book-store/api/database"
 	"github.com/codingsluv/book-store/api/internal/routes"
@@ -9,6 +10,12 @@ import (
 
 func main() {
 	app := fiber.New()
+	// CORS middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:8080",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+	}))
 	// database
 	database.InitDB()
 	// routes
