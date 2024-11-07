9619d111674c3763221012927bedd42eda0e8cc6
MuhammadNur <delusionboys974@gmail.com>
2024-11-07 23:50:46 +0700

    add CORS middleware

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -12,7 +12,7 @@ func main() {
 	app := fiber.New()
 	// CORS middleware
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:8080",
+		AllowOrigins: "http://localhost:5173",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 	}))
