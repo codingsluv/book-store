cf8adab1921a2af37e4d7cff4ce37fcf7b150e09
MuhammadNur <delusionboys974@gmail.com>
2024-12-20 09:31:05 +0700

    feat: implementation of new news cards
    This commit implements new news cards with the following features:
    
    - News title links to the news detail page.
    - Added a horizontal separator below the news title.
    - Vertical layout for mobile view.
    - Image for desktop view.

--- frontend/src/pages/home/News.jsx
+++ frontend/src/pages/home/News.jsx
@@ -4,53 +4,54 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Navigation, Pagination } from "swiper/modules";
 import news from "../../data/news";
+import { Link } from "react-router";
 
 export default function News() {
     return (
         <div className="py-16">
-            <h2 className="text-3xl font-semibold mb-6">News</h2>
+        <h2 className="text-3xl font-semibold mb-6">News</h2>
 
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={30}
-                navigation={true}
-                breakpoints={{
-                    640: {
-                        slidesPerView: 1,
-                        spaceBetween: 20,
-                    },
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 40,
-                    },
-                    1024: {
-                        slidesPerView: 2,
-                        spaceBetween: 50,
-                    },
-                    1180: {
-                        slidesPerView: 3,
-                        spaceBetween: 50,
-                    },
-                }}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-            >
-                {/* news list */}
-                {news.map((news, index) => (
-                    <SwiperSlide key={index}>
-                        <div className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4">
-                            <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
-                                <img src={news.image} alt="" className="w-full w-200 h-300 overflow-hidden flex justify-center bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200 size-50" />
-                            </div>
-
-                            <div>
-                                <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">{news.title}</h3>
-                                <p className="text-gray-600 mb-5">{news.description}</p>
-                            </div>
+        <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            navigation={true}
+            breakpoints={{
+                640: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 40,
+                },
+                1024: {
+                    slidesPerView: 2,
+                    spaceBetween: 50,
+                },
+            }}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+        >
+            {/* news list */}
+            {news.map((news, index) => (
+                <SwiperSlide key={index}>
+                    <div className='flex flex-col sm:flex-row sm:justify-between items-center gap-12'>
+                        {/* content */}
+                        <div className='py-4'>
+                            <Link to="/">
+                                 <h3 className='text-lg font-medium hover:text-blue-500 mb-4'>{news.title}</h3>
+                            </Link>
+                            <div className='w-12 h-[4px] bg-primary mb-5'></div>
+                            <p className='text-sm text-gray-600'>{news.description}</p>
                         </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
+
+                        <div className='flex-shrink-0'>
+                            <img src={news.image} alt=""  className='w-full object-cover'/>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    </div>
     )
 }
